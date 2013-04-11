155c18305f5ee1c7cf8b468d6c5f4c5b24c38a91
napster99 <morrisonleisure@163.com>
2013-04-11 16:58:56 +0800

    jquery-cboxlist.css & jquery-cboxlist.js modified

--- cblist/jquery-cboxlist.js
+++ cblist/jquery-cboxlist.js
@@ -4,75 +4,79 @@
   * date    : 2013-04-09
   * author  : Napster
   */
-$.fn.extend({
-	'cbinit' : function(data,callback) {
+(function($) {       
 
-		$(this).renderArea(data);
+	$.fn.extend({
+		'cbinit' : function(data,callback) {
+
+			$(this).renderArea(data);
 
 
-		$('.btn').find('a').click(function(){
-			var name = $(this).attr('class');
-			switch(name){
-				case 'sure':	
-					callback($(this).getCBData());
-					break;
-				case 'cancel':
-					$('.main-area').hide();
-					$('.list-area').find('input').attr('checked',false);
-					break;
-				default:
-					//do nothing
-			}
-		});
+			$('.btn').find('a').click(function(){
+				var name = $(this).attr('class');
+				switch(name){
+					case 'sure':	
+						callback($(this).getCBData());
+						break;
+					case 'cancel':
+						$('.main-area').hide();
+						$('.list-area').find('input').attr('checked',false);
+						break;
+					default:
+						//do nothing
+				}
+			});
 
-		$('.tMenu').click(function(){
-			$('.main-area').show();
-		});
+			$('.tMenu').click(function(){
+				$('.main-area').show();
+			});
 
-	},
+		},
 
-	'renderArea' : function(data){
-		var htmlStr = '<div style="width:180px; font-family:Microsoft Yahei;">'+
-							'<div class="tMenu">'+
-								'<span class="text">全部商店</span>'+
-								'<span class="icon">▼</span>'+
-							'</div>'+
-							'<div class="main-area" style="display:none;">'+
-								'<ul class="list-area">'+
-								'<li class="list-item" style="text-align:center;">暂无商店</li>'+
-								'</ul>'+
-								'<div class="btn">'+
-									'<a class="sure" href="javascript:;">确定</a>'+
-									'<a class="cancel" href="javascript:;">取消</a>'+
+		'renderArea' : function(data){
+			var htmlStr = '<div style="width:180px; font-family:Microsoft Yahei;">'+
+								'<div class="tMenu">'+
+									'<span class="text">全部商店</span>'+
+									'<span class="icon">▼</span>'+
 								'</div>'+
-							'</div>'+
-						'<div>';
-		$(this).html(htmlStr).setCBData(data);
-	},
+								'<div class="main-area" style="display:none;">'+
+									'<ul class="list-area">'+
+									'<li class="list-item" style="text-align:center;">暂无商店</li>'+
+									'</ul>'+
+									'<div class="btn">'+
+										'<a class="sure" href="javascript:;">确定</a>'+
+										'<a class="cancel" href="javascript:;">取消</a>'+
+									'</div>'+
+								'</div>'+
+							'<div>';
+			$(this).html(htmlStr).setCBData(data);
+		},
 
-	'getCBData' : function(){
-		var objData = [];
-		$('.list-area').find('li').each(function(){
-			if($(this).children('input').attr('checked')){
-				objData.push($(this).attr('storeId'));
+		'getCBData' : function(){
+			var objData = [];
+			$('.list-area').find('li').each(function(){
+				if($(this).children('input').attr('checked')){
+					objData.push($(this).attr('storeId'));
+				}
+			});
+
+			return objData.join(',');
+
+		},
+		'setCBData' : function(data){
+			if(data && data.constructor === Array){
+				var htmlStr = '';
+				for(var i=0; i<data.length; i++){
+					var storeId = data[i]['storeId']
+						,storeName = data[i]['storeName'];
+					htmlStr += '<li class="list-item" storeId="'+storeId+'"><input type="checkbox" />'+storeName+'</li>';
+				}
+				$('.list-area').html(htmlStr);
+			}else{
+				$('.list-area').html('<li class="list-item">暂无商店</li>');
 			}
-		});
 
-		return objData.join(',');
-
-	},
-	'setCBData' : function(data){
-		if(data && data.constructor === Array){
-			var htmlStr = '';
-			for(var i=0; i<data.length; i++){
-				var storeId = data[i]['storeId']
-					,storeName = data[i]['storeName'];
-				htmlStr += '<li class="list-item" storeId="'+storeId+'"><input type="checkbox" />'+storeName+'</li>';
-			}
-			$('.list-area').html(htmlStr);
-		}else{
-			$('.list-area').html('<li class="list-item">暂无商店</li>');
 		}
+	});
 
-	}
-});
+})(jQuery);    
