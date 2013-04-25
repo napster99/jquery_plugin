a82d6216848cd17601d733e951b74d1d1842ec77
napster99 <morrisonleisure@163.com>
2013-04-25 23:05:29 +0800

    pic3dloop modify

--- jqeury-pic3dloop/js/jquery-picloop.js
+++ jqeury-pic3dloop/js/jquery-picloop.js
@@ -18,24 +18,34 @@
 		opts['angle'] = 360/len;
 		// console.log(opts);
 
-		var opsCoordinate = 
-			[{'top':300,'left':350,'width':200,'height':300},
-			{'top':225,'left':750,'width':100,'height':150},
-			{'top':75,'left':400,'width':100,'height':150},
-			{'top':225,'left':0,'width':100,'height':150}];
 
 		init();
 
 		function init() {
+			getOpsCoordinate()
 			createContainer();
+			panelInit();
+			
+		};
 
+		//布局初始化
+		function panelInit() {
 			var imgs = $('#napster').find('img');
 
 			for(var i=0, len=imgs.length; i<len ; i++) {
-				imgs.eq(i).animate(opsCoordinate[i],1000).attr('css',opsCoordinate[i]);
+				imgs.eq(i).animate(opts.opsCoordinate[i],1000).attr('css',opts.opsCoordinate[i]);
 			}
-		};
+		}
 
+		//计算坐标点位置
+		function getOpsCoordinate() {
+			opts.opsCoordinate = [{'top':300,'left':350,'width':200,'height':300},
+			{'top':225,'left':750,'width':100,'height':150},
+			{'top':75,'left':400,'width':100,'height':150},
+			{'top':225,'left':0,'width':100,'height':150}];
+
+		}
+		//创建容器
 		function createContainer() {
 			var $div = $('<div  id="napster"></div>');
 			$div.css({
@@ -56,57 +66,49 @@
 			opts['container'].append($div);
 		};
 
+		//前一张
+		function backRun() {
+			var imgs = $('#napster').find('img');
+				for(var i=0,j=0; i<4; j++,i++) {
+					var left = imgs.eq(i+1).css('left'),
+						top = imgs.eq(i+1).css('top'),
+						width = imgs.eq(i+1).css('width'),
+						height = imgs.eq(i+1).css('height');
+					var ops = {
+						left : left,
+						top : top,
+						width : width,
+						height : height
+					}	
+
+					if(i == 3){
+						ops = {
+							left : imgs.eq(0).css('left'),
+							top : imgs.eq(0).css('top'),
+							width : imgs.eq(0).css('width'),
+							height : imgs.eq(0).css('height')
+						}	
+						imgs.eq(i).animate(ops);
+						continue;
+					}
+					imgs.eq(i).animate(ops);
+				}
+			}
+
+		//后一张
+		function forwardRun() {
+
+		}
+
 		$('button[name=up]').click(function() {
 			
-			var imgs = $('#napster').find('img');
-			for(var i=0,j=0; i<4; j++,i++) {
-				var left = imgs.eq(i+1).css('left'),
-					top = imgs.eq(i+1).css('top'),
-					width = imgs.eq(i+1).css('width'),
-					height = imgs.eq(i+1).css('height');
-				var ops = {
-					left : left,
-					top : top,
-					width : width,
-					height : height
-				}	
-
-				if(i == 3){
-					ops = {
-						left : imgs.eq(0).css('left'),
-						top : imgs.eq(0).css('top'),
-						width : imgs.eq(0).css('width'),
-						height : imgs.eq(0).css('height')
-					}	
-					imgs.eq(i).animate(ops);
-					continue;
-				}
-				
-
-				imgs.eq(i).animate(ops);
-			}
+			backRun();
 		});
 
-		$('button[name=down]').click(function() {
-			
-		});
+		
 	};
 
 })(jQuery);
 
-// var arrs = [a,b,c,d];
-
-// <img index=0 f=a />
-// <img index=1 f=b />
-// <img index=2 f=c />
-// <img index=3 f=d />
-
-// for(var i=0; i<imgs.length; i++) {
-// 	if(i == imgs.length -1){
-// 		imgs.eq(i).css('f',imgs.eq(0).attr('f')).attr('f',eq(0).attr('f'));
-// 	}
-
-// 	imgs.eq(i).css('f',imgs.eq(i+1).attr('f')).attr('f',eq(i+1).attr('f'));
-// }
 
 
